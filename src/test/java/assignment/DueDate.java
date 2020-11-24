e2ac8e2a123050aec0ae1d19a1529ad237b78c09
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-24 19:51:10 +0000

    Fixd date bug

--- src/test/java/assignment/DueDate.java
+++ src/test/java/assignment/DueDate.java
@@ -8,7 +8,7 @@ public class DueDate {
     public static boolean onTime(int year, int month, int day){
     Calendar cal = Calendar.getInstance();
     Calendar due = Calendar.getInstance();
-    due.set(year - 1900, month - 1, day, 22, 0);
+    due.set(year, month - 1, day, 22, 0);
     return cal.getTime().compareTo(due.getTime()) < 0;
     }
     
